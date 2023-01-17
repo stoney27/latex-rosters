b7516ce2a8768d138544daffd56ab94a85f8bdf9
stoney27 <stoney27@gmail.com>
2023-01-17 14:25:19 -0500

    Refactor -- tests
    I pulled out the write CSV file part of the main function, created
    a check arguments function, and I now have a method to check if we
    have more photos on disk than in our list.  The photo check function
    is to cover players being added to the roster after I receive that
    roster.  Also, I have written more tests for each new function.

--- src/create_mm_csv_data.py
+++ src/create_mm_csv_data.py
@@ -11,6 +11,7 @@
 import csv
 import sys
 import os
+import argparse
 import pprint
 
 pp = pprint.PrettyPrinter(indent=4)
@@ -39,73 +40,156 @@ def find_team_photo(photo_list):
 
     return(None)
 
+# Write out the new CSV file
+# new_csv_file, player_data, photos_list, team_photo, team_name
+def write_csv_file(csv_file, Pdata, Pphotos, team_photo, team_name):
+    '''Writes the CSV file'''
+
+    with open(csv_file, "w") as mm_csv:
+        neworder = ['Lastname', 'Firstname',
+                    'Sweater', 'Team', 'Photo', 'TeamPhoto']
+        csv_writer = csv.DictWriter(mm_csv, fieldnames=neworder)
+        csv_writer.writeheader()
+
+        for row in Pdata:
+
+            # if team name is not None, use it
+            if team_name is not None:
+                row["Team"] = team_name
+
+            # look for player photos:
+            for player in Pphotos:
+                if row["Lastname"] in player and row["Firstname"] in player:
+                    row["Photo"] = player
+
+            # if we didn't find a photo, use the blank photo
+            if row["Photo"] is None:
+                print("Error: could not find player photo: {}".format(
+                    row["Lastname"]))
+                row["Photo"] = "blank"
+
+            row["TeamPhoto"] = team_photo
+            csv_writer.writerow(row)
+
+
+# Get the command line arguments
+def get_check_args():
+    '''check the command line arguments'''
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("csv_file", help="CSV file to process")
+    arg_parser.add_argument("-t", "--team_name", help="Team name")
+    arg_parser.add_argument("-p", "--path", help="Path to photos")
+    args = arg_parser.parse_args()
+
+    # if no path is given, use the current directory name
+    if args.path is None:
+        path = os.getcwd()
+    else:
+        # trim off any leading spaces
+        args.path = args.path.strip()
+        path = args.path
+
+    if args.team_name is not None:
+        args.team_name = args.team_name.strip()
+    # Check to see if csv_file exists
+    if not os.path.isfile(args.csv_file):
+        print("ERROR: CSV file does not exist: {}".format(args.csv_file))
+        exit(1)
+
+    print(f"_Path = {path}")
+    print(f"_Filename = {args.csv_file}")
+    print(f"_Team name = {args.team_name}")
+
+    return args.csv_file, path, args.team_name
+
+# Make sure the list of photos match the player data and we don't have
+# more photos then players in the player data
+def check_photos(player_data, photos_list):
+    '''Check to see if the photos match the player data'''
+
+    # Get the number of players
+    num_players = len(player_data)
+
+    for photo in photos_list:
+        found = False
+        for row in player_data:
+            if row["Lastname"] in photo and row["Firstname"] in photo and row["Sweater"] in photo:
+                # Count the number of players found
+                num_players -= 1
+                found = True
+                break
+
+        if not found:
+            print(f"ERROR: Could not find player for photo: {photo}")
+            return False
+
+    if num_players != 0:
+        print(f"ERROR: Number of players in player data does not match number of photos: {num_players}")
+        # find the missing player photos
+        for player in player_data:
+            found = False
+            for photo in photos_list:
+                if player["Lastname"] in photo and player["Firstname"] in photo and player["Sweater"] in photo:
+                    found = True
+                    break
+            if not found:
+                print(f"Missing player photo: {player}")
+        return False
+
+    return True
+
+
 #--------------------------------------------------------------
 
 # Main function
 def main():
-    if len(sys.argv) != 4:
-        print("Inputs needed team name, csv file, and path to photos")
-        exit(1)
-
-    # Get the team name, csv file, and path to photos
-    team_name = sys.argv[1]
-    filename= sys.argv[2]
-    path = sys.argv[3]
-
+    '''Main function'''
+    filename, path, team_name = get_check_args()
+    
     print("Path = {}".format(path))
     print("Filename = {}".format(filename))
 
-    file_names = photos_full_path(path)
+    photos_list = photos_full_path(path)
 
-    team_photo = find_team_photo(file_names)
+    team_photo = find_team_photo(photos_list)
+    print(f"Team photo: {team_photo}")
     if team_photo is None:
         print("ERROR: Could not find team photo, exiting...")
         exit(1)
 
-    file_names.remove(team_photo)
-    print("Team photo: {}".format(team_photo))
-
+    photos_list.remove(team_photo)
+    
     #
-    player_data = csv.DictReader(open(filename))
-    field_names = player_data.fieldnames
+    data = csv.DictReader(open(filename))
+    field_names = data.fieldnames
+    player_data = list(data)
+
+    # check for all the fields we need
+    for field in ['Lastname', 'Firstname', 'Sweater']:
+        if field not in field_names:
+            print(f"ERROR: Missing field: {field}, exiting...")
+            exit(1)
+
+    # Check to see if Team is in the field names
+    if "Team" not in field_names:
+        # Make sure we have a team name from args
+        if team_name is None:
+            print("ERROR: No team name found, exiting...")
+            exit(1)
+
+    # Check to see if player_data match all photos_list
+    if not check_photos(player_data, photos_list):
+        print("ERROR: Photos do not match player data, exiting...")
+        exit(1)
 
     newfile = os.path.splitext(os.path.basename(filename))[0] + "_mm.csv"
     csv_file_path = os.path.dirname(filename)
 
     new_csv_file = csv_file_path + '/' + newfile
 
-    with open(new_csv_file, "w") as new_csv:
+    write_csv_file(new_csv_file, player_data, photos_list, team_photo, team_name)
 
-        try:
-            field_names.remove("")
-        except:
-            pass
-            
-        field_names.append("Teamname")
-        field_names.append("Photo")
-        
-        # if the field name Team is not in the list, add it
-        if "Team" not in field_names:
-            field_names.append("Team")
-
-        neworder = ['Lastname', 'Firstname', 'Sweater', 'Teamname', 'Photo', 'Team']
-        csv_writer = csv.DictWriter(new_csv,fieldnames=neworder)
-        csv_writer.writeheader()
-
-        for row in player_data:
-            row["Teamname"] = team_name
-
-            # look for player photos:
-            for player in file_names:
-                if row["Lastname"] in player and row["Firstname"] in player:
-                    row["Photo"] = player
-
-            if row["Photo"] is None:
-                print("Error: could not find player photo: {}".format(row["Lastname"]))
-                row["Photo"] = "blank"
-            
-            row["Team"] = team_photo
-            csv_writer.writerow(row)
+    print("Done!")
 
 
 if __name__ == "__main__":
--- tests/memory_mate_test.py
+++ tests/memory_mate_test.py
@@ -1,6 +1,36 @@
 import os
+import sys
+import pytest
+import csv
+
 from src.create_mm_csv_data import photos_full_path
 from src.create_mm_csv_data import find_team_photo
+from src.create_mm_csv_data import get_check_args
+from src.create_mm_csv_data import check_photos
+from src.create_mm_csv_data import write_csv_file
+from src.create_mm_csv_data import main
+
+
+# Create test csv data file
+def create_test_csv_data(tmpdir, filename):
+    # Create a temporary file
+    csv_file = os.path.join(tmpdir, filename)
+
+    file = open(csv_file, 'w', newline='')
+
+    with file:
+        # Create some test data with a temporary file
+        order = ['Firstname', 'Lastname', 
+                    'Sweater', 'Team']
+        csv_writer = csv.DictWriter(file, fieldnames=order)
+        csv_writer.writeheader()
+
+        # Create some test data
+        csv_writer.writerow({'Firstname': 'John', 'Lastname': 'Smith','Sweater': '45', 'Team': '12U AAA'})
+        csv_writer.writerow({'Firstname': 'Jerry', 'Lastname': 'Flynn','Sweater': '89', 'Team': '12U AAA'})
+        csv_writer.writerow({'Firstname': 'Ian', 'Lastname': 'Stone','Sweater': '12', 'Team': '12U AAA'})
+
+    return csv_file
 
 # Test photos_full_path function
 def test_photos_full_path(tmpdir):
@@ -30,3 +60,225 @@ def test_find_team_photo():
     # Call the function under test
     assert find_team_photo(photo_list) == '/tmp/Team Photo - 12U AAA.jpg'
     assert find_team_photo(photo_list2) == None
+
+# Test get_check_args function
+def test_get_check_args(tmpdir):
+
+    # set the sys.argv list
+    # Create a temporary file
+    csv_file = os.path.join(tmpdir, 'test.csv')
+
+    sys.argv = ['create_mm_csv_data.py', csv_file, '-p /tmp', '-t "Team Name"']
+
+    # Create some test data with a temporary file
+    open(csv_file, 'w').close()
+    filename = csv_file
+    path = '/tmp'
+    team_name = '"Team Name"'
+
+    # Call the function under test
+    assert get_check_args() == (filename, path, team_name)
+
+    # Call with just two args
+    sys.argv = ['create_mm_csv_data.py', csv_file]
+    # get current working directory
+    cwd = os.getcwd()
+    assert get_check_args() == (filename, cwd, None)
+
+    # Call with just one arg
+    sys.argv = ['create_mm_csv_data.py']
+    with pytest.raises(SystemExit) as e:
+        get_check_args()
+    assert e.type == SystemExit
+    assert e.value.code == 2
+
+    # Call with no filename but other args
+    sys.argv = ['create_mm_csv_data.py', '-p /tmp', '-t "Team Name"']
+    with pytest.raises(SystemExit) as e:
+        get_check_args()
+    assert e.type == SystemExit
+    assert e.value.code == 2
+
+def test_check_photos(tmpdir):
+
+    csv_file = create_test_csv_data(tmpdir, 'test.csv')
+    print(f"csv_file: {csv_file}")
+
+    data = csv.DictReader(open(csv_file))
+    
+    player_data = list(data)
+
+    # Create a list of photo files including playernames in the filename
+    photo_list = []
+    photo_list.append(os.path.join(tmpdir, 'John Smith#45.jpg'))
+    photo_list.append(os.path.join(tmpdir, 'Jerry Flynn#89.jpg'))
+
+    # Call the function under test
+    assert check_photos(player_data,photo_list) == False
+
+    # Create a list of photo files including playernames in the filename
+    photo_list = []
+    photo_list.append(os.path.join(tmpdir, 'John Smith#45.jpg'))
+    photo_list.append(os.path.join(tmpdir, 'Jerry Flynn#89.jpg'))
+    photo_list.append(os.path.join(tmpdir, 'Ian Stone#12.jpg'))
+
+    # Call the function under test
+    assert check_photos(player_data,photo_list) == True
+
+# Test write_csv_file function
+def test_write_csv_file(tmpdir):
+
+    # setup data
+    csv_file = create_test_csv_data(tmpdir, 'test.csv')
+    data = csv.DictReader(open(csv_file, 'r'))
+    field_names = data.fieldnames
+
+    player_data = list(data)
+
+    # Create a temporary file
+    csv_file = os.path.join(tmpdir, 'newData.csv')
+
+    team_photo = "/tmp/Team Photo - 12U AAA.jpg"
+
+    photo_list = [
+        '/tmp/John Smith#45.jpg',
+        '/tmp/Jerry Flynn#89.jpg',
+        '/tmp/Ian Stone#12.jpg',
+    ]
+    # Call the function under test
+    write_csv_file(csv_file, player_data, photo_list, team_photo, None)
+
+    # Read the file and check the data
+    data = csv.DictReader(open(csv_file))
+    field_names = data.fieldnames
+    assert field_names == ['Lastname', 'Firstname', 'Sweater', 'Team', 'Photo', 'TeamPhoto']
+    test_player_data = list(data)
+    assert test_player_data[0]['Firstname'] == 'John'
+    assert test_player_data[0]['Lastname'] == 'Smith'
+    assert test_player_data[0]['Sweater'] == '45'
+    assert test_player_data[0]['Team'] == '12U AAA'
+    assert test_player_data[0]['Photo'] == '/tmp/John Smith#45.jpg'
+    assert test_player_data[0]['TeamPhoto'] == '/tmp/Team Photo - 12U AAA.jpg'
+    assert test_player_data[2]['Lastname'] == 'Stone'
+    assert test_player_data[2]['Firstname'] == 'Ian'
+    assert test_player_data[2]['Sweater'] == '12'
+    assert test_player_data[2]['Team'] == '12U AAA'
+    assert test_player_data[2]['Photo'] == '/tmp/Ian Stone#12.jpg'
+    assert test_player_data[2]['TeamPhoto'] == '/tmp/Team Photo - 12U AAA.jpg'
+
+    # Call write_csv_file with team name
+    write_csv_file(csv_file, player_data, photo_list, team_photo, "16U AAA")
+
+    # Read the file and check the data
+    data = csv.DictReader(open(csv_file))
+    field_names = data.fieldnames
+    assert field_names == ['Lastname', 'Firstname', 'Sweater', 'Team', 'Photo', 'TeamPhoto']
+    test_player_data = list(data)
+    assert test_player_data[0]['Firstname'] == 'John'
+    assert test_player_data[0]['Team'] == '16U AAA'
+
+
+def test_main(mocker, tmpdir):
+    # Set up test variables and mocked functions
+    csv_file = create_test_csv_data(tmpdir, 'test.csv')
+    
+    sys.argv = ['create_mm_csv_data.py', csv_file, '-p /tmp', '-t "12U AAA"']
+
+    path = "test_path"
+    team_name = "12U AAA"
+    team_photo = "/tmp/Team Photo - 12U AAA.jpg"
+
+    photos_list = [
+        '/tmp/John Smith#45.jpg',
+        '/tmp/Jerry Flynn#89.jpg',
+        '/tmp/Ian Stone#12.jpg',
+        '/tmp/Team Photo - 12U AAA.jpg'
+    ]
+
+    # Define mocked functions
+    mocker.patch('src.create_mm_csv_data.get_check_args', return_value=(csv_file, path, team_name))
+    mocker.patch('src.create_mm_csv_data.photos_full_path', return_value=photos_list)
+    mocker.patch('src.create_mm_csv_data.find_team_photo', return_value=team_photo)
+    mocker.patch('src.create_mm_csv_data.check_photos', return_value=True)
+
+    # Call main function
+    main()
+
+    expected_player_data = [{'Lastname': 'Smith', 'Firstname': 'John', 'Sweater': '45', 'Team': '12U AAA', 'Photo': '/tmp/John Smith#45.jpg', 'TeamPhoto': '/tmp/Team Photo - 12U AAA.jpg'}, 
+                            {'Lastname': 'Flynn', 'Firstname': 'Jerry', 'Sweater': '89','Team': '12U AAA', 'Photo': '/tmp/Jerry Flynn#89.jpg', 'TeamPhoto': '/tmp/Team Photo - 12U AAA.jpg'},
+                            {'Lastname': 'Stone', 'Firstname': 'Ian', 'Sweater': '12', 'Team': '12U AAA', 'Photo': '/tmp/Ian Stone#12.jpg', 'TeamPhoto': '/tmp/Team Photo - 12U AAA.jpg'}]
+    # Check that new file is created
+    newfile = os.path.splitext(os.path.basename(csv_file))[0] + "_mm.csv"
+    csv_file_path = os.path.dirname(csv_file)
+    new_csv_file = os.path.join(csv_file_path, newfile)
+    assert os.path.exists(new_csv_file)
+
+    # Check that new file contains expected data
+    with open(new_csv_file) as f:
+        reader = csv.DictReader(f)
+        rows = list(reader)
+        assert rows == expected_player_data
+
+    # Clean up
+    os.remove(new_csv_file)
+    os.remove(csv_file)
+
+    # Test with no team photo
+    mocker.patch('src.create_mm_csv_data.find_team_photo', return_value=None) 
+    
+    with pytest.raises(SystemExit) as e:
+        main()
+    assert e.type == SystemExit
+    assert e.value.code == 1
+
+
+    # Test with different team name
+    csv_file2 = create_test_csv_data(tmpdir, 'test2.csv')
+    team_name = "16U Black"
+    sys.argv = ['create_mm_csv_data.py', csv_file2, '-p /tmp', '-t ' + team_name]
+
+    path = "test_path"
+    team_photo = "/tmp/Team Photo.jpg"
+
+    photos_list = [
+        '/tmp/John Smith#45.jpg',
+        '/tmp/Jerry Flynn#89.jpg',
+        '/tmp/Ian Stone#12.jpg',
+        '/tmp/Team Photo.jpg'
+    ]
+
+    # Define mocked functions
+    mocker.patch('src.create_mm_csv_data.get_check_args',
+                return_value=(csv_file2, path, team_name))
+    mocker.patch('src.create_mm_csv_data.photos_full_path',
+                return_value=photos_list)
+    mocker.patch('src.create_mm_csv_data.find_team_photo',
+                return_value=team_photo)
+    mocker.patch('src.create_mm_csv_data.check_photos', return_value=True)
+
+    # Call main function
+    main()
+
+    newfile = os.path.splitext(os.path.basename(csv_file2))[0] + "_mm.csv"
+    csv_file_path2 = os.path.dirname(csv_file2)
+    new_csv_file = os.path.join(csv_file_path2, newfile)
+    assert os.path.exists(new_csv_file)
+
+    print(f"new_csv_file: {new_csv_file}")
+
+    expected_player_data2 = [{'Lastname': 'Smith', 'Firstname': 'John', 'Sweater': '45', 'Team': '16U Black', 
+                                'Photo': '/tmp/John Smith#45.jpg', 'TeamPhoto': '/tmp/Team Photo.jpg'},
+                            {'Lastname': 'Flynn', 'Firstname': 'Jerry', 'Sweater': '89', 'Team': '16U Black',
+                                'Photo': '/tmp/Jerry Flynn#89.jpg', 'TeamPhoto': '/tmp/Team Photo.jpg'},
+                            {'Lastname': 'Stone', 'Firstname': 'Ian', 'Sweater': '12', 'Team': '16U Black', 
+                                'Photo': '/tmp/Ian Stone#12.jpg', 'TeamPhoto': '/tmp/Team Photo.jpg'}]
+    
+    # Check that new file contains expected data
+    with open(new_csv_file) as f:
+        reader = csv.DictReader(f)
+        rows = list(reader)
+        assert rows == expected_player_data2
+
+    # Clean up test file
+    os.remove(new_csv_file)
+    os.remove(csv_file2)
